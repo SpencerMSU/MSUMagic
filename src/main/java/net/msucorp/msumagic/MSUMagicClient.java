599173a74f9a6cb685fbe5442dd25189e308bfcb
SpencerMSU <rjdcbxxhcx@gmail.com>
2025-01-25 13:18:34 +0300

    some code added/removed

---
+++ src/main/java/net/msucorp/msumagic/MSUMagicClient.java
@@ -0,0 +1,11 @@
+package net.msucorp.msumagic;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.block.entity.VaultBlockEntity;
+
+public class MSUMagicClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
